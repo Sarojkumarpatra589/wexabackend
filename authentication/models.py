b2acf0198b0392b4a7997359dc681e998679981f
Saroj Kumar Patra <12345678+Sarojkumarpatra589@users.noreply.github.com>
2026-05-22 21:10:41 +0530

    new

--- authentication/models.py
+++ authentication/models.py
@@ -1,19 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-class User(AbstractUser):
-    ROLE_CHOICES = (
-        ("admin", "Admin"),
-        ("hr", "HR"),
-        ("employee", "Employee"),
-    )
-
-    email = models.EmailField(unique=True)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="employee")
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
-
-    def __str__(self):
-        return self.email
+# Create your models here.
